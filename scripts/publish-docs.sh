4795753addb88b2d6c1170c50f9756b76657706e
Diego Lemos <diegolemosderesende@gmail.com>
2020-06-04 23:02:54 +0100

    Fix publish docs script

--- scripts/publish-docs.sh
+++ scripts/publish-docs.sh
@@ -4,11 +4,15 @@ set -e # bail out early if any command fails
 set -u # fail if we hit unset variables
 set -o pipefail # fail if any component of any pipe fails
 
-REPO_DIR="$(dirname "$0")"
+REPO_DIR="$(cd $(dirname "$0") && pwd)/.."
 
-make docs > wiki/Home.md
-( cd wiki
-  git add Home.md
-  git commit -m "Update cheatsheet"
-  git push
+( cd "$REPO_DIR"
+  make docs > wiki/Home.md
+
+  ( cd wiki
+    git pull --rebase
+    git add Home.md
+    git commit -m "Update cheatsheet"
+    git push
+  )
 )
